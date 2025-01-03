847b3b1009217d33fe6e3df25676b8fbd9f3bab3
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-01-03 14:59:02 -0300

    agregar pedidos

--- app/Http/Controllers/Pedido/PedidosController.php
+++ app/Http/Controllers/Pedido/PedidosController.php
@@ -4,10 +4,8 @@ namespace App\Http\Controllers\Pedido;
 
 use App\Http\Controllers\Controller;
 use App\Models\Pedido;
-use App\Models\PedidoItem;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
 
 class PedidosController extends Controller
