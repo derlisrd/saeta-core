a0e393a6e2fcfbd05c674fec1404ebde01ea4cab
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-01-22 15:37:35 -0300

    ruc paraguay

--- app/Services/RucParaguayService.php
+++ app/Services/RucParaguayService.php
@@ -12,20 +12,24 @@ class RucParaguayService{
 
     public function infoRuc($ruc)
     {
+        $apikey = env('API_KEY_RUC_PARAGUAY');
         $url = env('URL_API_RUC_PARAGUAY');
-        $url = $url . "/contribuyente" . "/".$ruc;
+        $url = $url . '/' .$ruc;
         $res = Http::get($url);
         $json = $res->json();
         // aqui modelar
         $data = (object) $json['data'];
-        $info = [
+        return $this->dto($data);
+    }
+
+
+    private function dto($data){
+        return [
             'doc'=> $data->doc,
             'nombre' => $data->razonSocial,
             'ruc' => $data->ruc,
             'estado'=>$data->estado,
             'juridico'=>$data->esPersonaJuridica ? 1 : 0
         ];
-        // aqui modelar
-        return $info;
     }
 }
