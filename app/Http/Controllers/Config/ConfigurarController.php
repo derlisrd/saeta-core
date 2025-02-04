964c7b98c449a9be9ee6e2fa33617d5db734e0a1
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-02-04 05:42:52 -0300

    venta pop

--- app/Http/Controllers/Config/ConfigurarController.php
+++ app/Http/Controllers/Config/ConfigurarController.php
@@ -3,15 +3,31 @@
 namespace App\Http\Controllers\Config;
 
 use App\Http\Controllers\Controller;
+use App\Models\Empresa;
 use Illuminate\Http\Request;
 
 class ConfigurarController extends Controller
 {
     public function verificar(){
+        $empresa = Empresa::first();
+        
+            return response()->json([
+                'success' => true,
+                'results' => [
+                    'configurado' => $empresa->configurado
+                ]
+            ]);
         
     }
 
     public function configurar(){
+        $empresa = Empresa::first();
+        $empresa->configurado = 1;
+        $empresa->save();
 
+        return response()->json([
+            'success' => true,
+            'message' => 'Empresa configurada correctamente'
+        ]);
     }
 }
