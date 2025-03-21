189c70bf48641a49ffc4d0e0d873b88aea9a0fd5
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-03-21 12:25:23 -0300

    formas de pago

---
+++ app/Models/FormasPagoPedido.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FormasPagoPedido extends Model
+{
+    use HasFactory;
+    protected $table = 'formas_pago_pedidos';
+    protected $fillable = [
+        'pedido_id',
+        'abreviatura',
+        'forma_pago_id',
+        'monto',
+        'porcentaje_descuento',
+        'monto_descuento',
+        'monto_total'
+    ];
+}
---
+++ database/migrations/2025_03_21_122134_create_formas_pago_pedidos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('formas_pago_pedidos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('pedido_id');
+            $table->unsignedBigInteger('forma_pago_id');
+            $table->string('abreviatura');
+            $table->decimal('monto', 10, 2);
+            $table->decimal('porcentaje_descuento', 10, 2);
+            $table->decimal('monto_descuento', 10, 2);
+            $table->decimal('monto_total', 10, 2);
+            $table->foreign('pedido_id')->references('id')->on('pedidos');
+            $table->foreign('forma_pago_id')->references('id')->on('formas_pagos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('formas_pago_pedidos');
+    }
+};
