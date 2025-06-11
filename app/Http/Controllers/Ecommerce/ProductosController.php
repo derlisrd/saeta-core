1310d77449443b4a78caf5accadaa094d6c2e7ad
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-06-11 16:22:38 -0300

    select nuevos

--- app/Http/Controllers/Ecommerce/ProductosController.php
+++ app/Http/Controllers/Ecommerce/ProductosController.php
@@ -10,7 +10,9 @@ class ProductosController extends Controller
 {
     public function index(){
         $productos = Producto::where('disponible', 1)
+        ->where('tipo',1)
         ->with('images')
+            ->select('id', 'nombre','codigo','precio_normal', 'descripcion', 'disponible','precio_descuento')
         ->get();
         return response()->json([
             'success' => true,
