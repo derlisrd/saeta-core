7ee86bb26d460307dc0051d3ffe36d518d8b3a94
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-06-11 14:43:16 -0300

    add: cuota

--- app/Models/Cuota.php
+++ app/Models/Cuota.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Cuota extends Model
 {
     protected $table = 'cuotas';
-    protected $fillable = ['credito_id','pedido_id','cliente_id','monto','obs'];
+    protected $fillable = ['user_id','credito_id','pedido_id','cliente_id','monto','obs'];
 }
