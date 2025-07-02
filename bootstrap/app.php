573210f3489abba47f799d960a38bb4d0d399185
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-07-02 11:59:56 -0300

    tennant

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -18,14 +18,20 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         api: __DIR__.'/../routes/api.php',
         //commands: __DIR__.'/../routes/console.php',
-        health: '/up',
-        apiPrefix:'/api',
+        //health: '/up',
+        //apiPrefix:'/api',
         then: function ($router) {
             
             Route::prefix('ecommerce')
             ->middleware(XapiKeyTokenIsValid::class)
             ->name('ecommerce')
             ->group(__DIR__ . '/../routes/ecommerce.php');
+
+            Route::middleware([
+                'api',
+                \Stancl\Tenancy\Middleware\InitializeTenancyByDomain::class,
+                \Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains::class,
+            ])->group(base_path('routes/tenant.php'));
         }
     )    ->withMiddleware(function (Middleware $middleware) {
         $middleware->api(append: [
