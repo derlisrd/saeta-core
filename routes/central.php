cab12903cd04536f51b8354287c8f3cb40f2fb5c
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-07-02 18:16:46 -0300

    ventas central

--- routes/central.php
+++ routes/central.php
@@ -5,6 +5,14 @@
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'Bienvenido a Saeta. Desde aquí se crean nuevas tiendas.'
+    ]);
+});
+
+
 Route::get('/status', function () {
     return ['status' => 'central ok', 'db' => DB::connection()->getDatabaseName()];
 });
