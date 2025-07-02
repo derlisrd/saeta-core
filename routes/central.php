e5e84bfea367a75a2b6f157c8a0a6dce46448d5e
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-07-02 18:31:09 -0300

    crear tenand

--- routes/central.php
+++ routes/central.php
@@ -16,3 +16,26 @@ Route::get('/', function () {
 Route::get('/status', function () {
     return ['status' => 'central ok', 'db' => DB::connection()->getDatabaseName()];
 });
+
+/* Route::get('/', function () {
+    // 1. Crear el tenant
+    $tenant = Tenant::create([
+        'id' => 'stock',
+    ]);
+    
+    // 2. Asignar el dominio
+    $tenant->domains()->create([
+        'domain' => 'stock.saeta.uk'
+    ]);
+
+    // 3. Inicializar el contexto tenant
+    tenancy()->initialize($tenant);
+
+    // 4. Ejecutar el seeder
+    (new ExampleSeeder)->run();
+
+    // 5. Finalizar el contexto tenant
+    tenancy()->end();
+
+    return response()->json(['message' => 'Tenant creado y seed ejecutado']);
+});  */
