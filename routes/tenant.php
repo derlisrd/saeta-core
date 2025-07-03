9ee4567e068352b7e0618c53cfb27614acd13967
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-07-02 23:41:40 -0300

    tiendas

--- routes/tenant.php
+++ routes/tenant.php
@@ -3,8 +3,25 @@
 
 // routes/tenant.php
 use Illuminate\Support\Facades\Route;
-
+use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
+use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
 Route::get('/', function () {
     return response()->json(['message' => 'E‑commerce de ' . tenant('id')]);
 });
+
+Route::domain('{tenant}.saeta.uk')                 // <── sólo subdominios
+     ->middleware([
+         'web',
+         InitializeTenancyByDomain::class,
+         PreventAccessFromCentralDomains::class,
+     ])
+     ->group(function () {
+         Route::get('/', function () {
+             return response()->json([
+                 'message' => 'home tienda de ' . tenant('id'),
+             ]);
+         });
+
+         // …todas las demás rutas públicas del ecommerce
+     });
