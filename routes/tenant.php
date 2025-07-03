c42d6743bb15bb8b4bb2c559b744ac1f7dc27fac
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-07-02 23:22:35 -0300

    ok

--- routes/tenant.php
+++ routes/tenant.php
@@ -3,21 +3,10 @@
 
 // routes/tenant.php
 use Illuminate\Support\Facades\Route;
-use Stancl\Tenancy\Middleware\InitializeTenancyByDomain;
-use Stancl\Tenancy\Middleware\PreventAccessFromCentralDomains;
 
-Route::domain('{tenant}.saeta.uk')                 // <── sólo subdominios
-     ->middleware([
-         'web',
-         InitializeTenancyByDomain::class,
-         PreventAccessFromCentralDomains::class,
-     ])
-     ->group(function () {
-         Route::get('/', function () {
-             return response()->json([
-                 'message' => 'home tienda de ' . tenant('id'),
-             ]);
-         });
 
-         // …todas las demás rutas públicas del ecommerce
-     });
+Route::get('/', function () {
+    return response()->json([
+        'message' => 'tenant ok'
+    ]);
+});
