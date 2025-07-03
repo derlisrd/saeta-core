c5c455bd2115ddc618ffccc30fb36c2669972ba8
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-07-02 23:05:58 -0300

    welcome

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,6 @@
 <?php
 
-use App\Http\Controllers\Pedido\PedidosController;
+
 use Illuminate\Support\Facades\Route;
 
-/* Route::any('/',function(){return response()->json(['success'=>false,'message'=>'Not found'],404);});
-Route::fallback(function () {return response()->json(['success'=>false,'message'=>'Not found route.'],404);}); */
-
 Route::view('/',['welcome']);
-
-Route::get('/recibo/{id}', [PedidosController::class, 'sendEmailRecibo']);
