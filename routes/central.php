ed3c5d1f8f1d55ab2a5af7f5978244ae6229ea9e
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-07-02 23:23:08 -0300

    central ok

--- routes/central.php
+++ routes/central.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json([
-        'message' => 'Bienvenido a Saeta. Desde aquí se crean nuevas tiendas.'
+        'message' => 'central ok'
     ]);
 });
 
