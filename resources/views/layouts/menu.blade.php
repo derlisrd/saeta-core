87188cb957fe6f99d1ef8ce3bd08804606acf019
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-07-07 23:13:13 -0300

    dashboard

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -7,12 +7,15 @@
 
     <div class="flex items-center space-x-4">
         @guest('admin')
-        <a href="{{ route('login') }}" class="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition duration-300 ease-in-out hidden sm:block">Iniciar Sesión</a>
+        <a href="{{ route('login') }}" class="text-gray-600 dark:text-gray-300 hover:text-blue-600 dark:hover:text-blue-400 transition duration-300 ease-in-out ">Iniciar</a>
         <a href="{{ route('register') }}" class="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-5 rounded-full shadow-md transition duration-300 ease-in-out">
             Registrar
         </a>
         @endguest
         @auth('admin')
+        <a href="{{ route('dashboard') }}" class="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-5 rounded-full shadow-md transition duration-300 ease-in-out">
+            Dashboard
+        </a>
         <a href="{{ route('logout') }}" class="bg-blue-600 hover:bg-blue-700 text-white font-semibold py-2 px-5 rounded-full shadow-md transition duration-300 ease-in-out">
             Cerrar Sesión
         </a>
