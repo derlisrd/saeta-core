6168eeecec2662476ac4a896dcc7153f0481275c
Derlis Ruiz Diaz <derlisruizdiaz@hotmail.com>
2025-07-07 23:39:54 -0300

    details

--- routes/ecommerce.php
+++ routes/ecommerce.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Ecommerce\HomeController;
 //use App\Http\Controllers\ViewsCentral\TiendaController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',[HomeController::class, 'index']);
-
+Route::get('/',[HomeController::class, 'index'])->name('e_inicio');
+Route::get('/details/{id}',[HomeController::class,'details'])->name('e_details');
 
 //Route::get('/reparar',[TiendaController::class,'ensureStorageLink']);
