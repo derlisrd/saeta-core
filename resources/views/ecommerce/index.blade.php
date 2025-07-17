23cf640730d0f30dd0cbaede3e68a4358804c8c6
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-07-17 16:45:00 -0300

    ndishhhh

--- resources/views/ecommerce/index.blade.php
+++ resources/views/ecommerce/index.blade.php
@@ -24,7 +24,7 @@
                     <div class="flex flex-row gap-2">
                         <div>
                             <img 
-                            class="h-24 rounded-xl object-cover w-full"
+                            class="h-full rounded-xl object-cover w-full min-w-24 max-h-24"
                             alt="{{ $producto->nombre }}"
                             src="{{ $producto->images->first()->miniatura ?? 'https://placehold.co/100x100.png' }}" />
                         </div>
