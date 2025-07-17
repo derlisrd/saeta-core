ae1f8c262ff883ea8984e5e240d3dde5257eb7ad
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-07-17 16:32:15 -0300

    register

--- resources/views/central/register.blade.php
+++ resources/views/central/register.blade.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registro - Mi Plataforma</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <title>Crear una cuenta</title>
+    @vite(['resources/css/app.css'])
     @livewireStyles
 </head>
 <body class='bg-gray-100 dark:bg-dark flex flex-col min-h-screen'>
