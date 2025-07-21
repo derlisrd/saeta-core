1ecd29535ce2bb13d5528347b1db1fb0e6d344e8
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-07-21 17:48:14 -0300

    a

--- resources/views/layouts/ecommerce.blade.php
+++ resources/views/layouts/ecommerce.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('header_title', 'Tienda Online - Inicio')</title>
 
-    {{-- <link rel="stylesheet" href="{{ env('APP_URL') }}/build/assets/app.css">  --}}
-     @vite(['resources/css/app.css']) 
+    <link rel="stylesheet" href="{{ env('APP_URL') }}/build/assets/app.css"> 
+    {{--  @vite(['resources/css/app.css'])  --}}
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;700&display=swap" rel="stylesheet">
     <style>
         body {
